a7ccef153a77412ef0d3fd5ae5516a3cfe15b186
mariam-moamen <mariam-moamen@hotmail.com>
2022-08-08 08:46:26 +0200

    add bottom navigation bar to home screen

--- lib/ui/features/pages/home-screen.dart
+++ lib/ui/features/pages/home-screen.dart
@@ -1,7 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:islami/ui/my_colors.dart';
 
-class HomeScreen extends StatelessWidget {
-  const HomeScreen({Key? key}) : super(key: key);
+class HomeScreen extends StatefulWidget {
+  HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  int selectedIndex = 0;
 
   @override
   Widget build(BuildContext context) {
@@ -12,21 +20,56 @@ class HomeScreen extends StatelessWidget {
         ),
       ),
       child: Scaffold(
-        backgroundColor: Colors.transparent,
-        appBar: AppBar(
-            title: const Text(
-          'Islami',
-          style: TextStyle(
-            color: Colors.black,
-            fontSize: 30,
-            fontWeight:FontWeight.w200
-          ),
-        ),
-          centerTitle: true,
           backgroundColor: Colors.transparent,
-          elevation: 0,
-        ),
-      ),
+          appBar: AppBar(
+            title: const Text(
+              'Islami',
+              style: TextStyle(
+                  color: Colors.black,
+                  fontSize: 30,
+                  fontWeight: FontWeight.w200),
+            ),
+            centerTitle: true,
+            backgroundColor: Colors.transparent,
+            elevation: 0,
+          ),
+          bottomNavigationBar: Theme(
+            data: Theme.of(context).copyWith(
+              canvasColor: MyColors.primaryColor,
+            ),
+            child: BottomNavigationBar(
+              currentIndex: selectedIndex,
+              onTap: (index) {
+                selectedIndex = index;
+                setState(() {});
+              },
+              selectedIconTheme: const IconThemeData(
+                color: Colors.black,
+              ),
+              unselectedIconTheme: const IconThemeData(
+                color: Colors.white,
+              ),
+              selectedLabelStyle: const TextStyle(
+                color: Colors.black,
+              ),
+              selectedItemColor: Colors.black,
+              unselectedItemColor: Colors.white,
+              items: const [
+                BottomNavigationBarItem(
+                    icon: ImageIcon(AssetImage('assets/images/quran.png')),
+                    label: 'Quran'),
+                BottomNavigationBarItem(
+                    icon: ImageIcon(AssetImage('assets/images/hadeth.png')),
+                    label: 'Hadeth'),
+                BottomNavigationBarItem(
+                    icon: ImageIcon(AssetImage('assets/images/sebha_gold.png')),
+                    label: 'Sebha'),
+                BottomNavigationBarItem(
+                    icon: ImageIcon(AssetImage('assets/images/radio.png')),
+                    label: 'Radio'),
+              ],
+            ),
+          )),
     );
   }
 }
---
+++ lib/ui/my_colors.dart
@@ -0,0 +1,5 @@
+import 'package:flutter/material.dart';
+
+class MyColors{
+  static const Color primaryColor=  Color(0xffB7935F);
+}
